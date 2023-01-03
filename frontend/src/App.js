56a0dc7c8a850707cf843daadc5968f7b98da6cc
Eloagbawe <eloagbawe@gmail.com>
2023-01-03 23:16:29 +0100

    update App.js and global styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import {Routes, Route} from 'react-router-dom'
@@ -7,20 +7,35 @@ import { useLocation } from 'react-router-dom'
 import {Home } from './pages/Home'
 import { Login } from './pages/Login';
 import { Signup } from './pages/Signup';
+import { Search } from './pages/Search';
+import { Dashboard } from './pages/Dashboard'
 
 import './App.css';
 
 function App() {
+  const location = useLocation()
+  useEffect(() => {
+    if (location.pathname === '/') {
+      document.body.classList.remove('bg-image-static')
+      document.body.classList.add('bg-image')
+    } else {
+      document.body.classList.add('bg-image-static')
+      document.body.classList.remove('bg-image')
+    }
+  }, [location])
 
   return (
     <>
     {/* <Router> */}
-    <div className={useLocation().pathname === '/' ? 'bg-image' : 'bg-image-static'}>
+    {/* <div className={useLocation().pathname === '/' ? 'bg-image' : 'bg-image-static'}> */}
+    <div className='pb-5'>
     <Header/>
     <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/login' element={<Login/>} />
         <Route path='/signup' element={<Signup/>} />
+        <Route path='/search' element={<Search/>} />
+        <Route path='/dashboard' element={<Dashboard/>} />
 
       </Routes>
     <Footer/>
