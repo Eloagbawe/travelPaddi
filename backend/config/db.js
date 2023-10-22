0264f302d6669f2d886eb03f5ab243a25e6ccd23
Eloagbawe <eloagbawe@gmail.com>
2023-10-22 19:27:58 +0100

    add super test package for testing and handle cors issues

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose')
 mongoose.set('strictQuery', true)
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI)
+        const mongo_uri = process.env.NODE_ENV === 'production' ? process.env.MONGO_URI : process.env.MONGO_URI_DEV
+        const conn = await mongoose.connect(mongo_uri)
 
         console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
         
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const colors = require('colors')
 const dotenv = require('dotenv').config();
 const multer = require('multer');
+const cors = require('cors')
 const {errorHandler} = require('./middleware/error')
 const connectDB = require('./config/db')
 const port = process.env.PORT || 5001
@@ -9,6 +10,7 @@ const port = process.env.PORT || 5001
 connectDB()
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(multer().array())
